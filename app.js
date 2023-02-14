f26225ec53131291a70d069c7a00d37cb031e670
Ahsan <ahsansiddiqui856@gmail.com>
2023-02-14 23:57:36 +0500

    first commit+

---
+++ app.js
@@ -0,0 +1,25 @@
+// 
+
+//  const sum1 = function(a,b){
+//     alert (a*b)
+
+//  }
+//  sum1(2,5)
+
+
+
+
+// function myfuction1(a,b) {
+//     alert(`${a + b}`)
+// }
+
+// myfuction1(1,2)
+
+// const myfuction2 = function(c,d) {
+//     alert(`${c + d}`)
+// }
+
+// myfuction2(3, 8)
+
+// const myfuction3 = (e, f) => alert(`${e + f}`)
+// myfuction3(5, 8)
